20d07160204f86952f3535c145a676139b36e37a
Sonia Singla <ssoniyaster@gmail.com>
2017-06-27 18:29:00 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,8 @@
+
+   function getwords() {
+     
+       text2 = word1.value;
+       document.getElementById("para").innerHTML += '<p>'+text2
+       document.getElementById("words").value = "enter"
+}
+ 
